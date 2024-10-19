a7f7d1ba7a80cf1674f191696bc2b3047167e8e1
joho2022 <josama2022.dev@gmail.com>
2024-10-20 02:46:18 +0900

    feat: HeaderView UI 구현
    - 토스 이미지 추가
    - UIView 재사용 메서드 추가
    - UIStackView 재사용 메서드 추가

---
+++ 35-Seminar/35-Seminar/View/AppHeaderView.swift
@@ -0,0 +1,122 @@
+//
+//  AppHeaderView.swift
+//  35-Seminar
+//
+//  Created by 조호근 on 10/12/24.
+//
+
+import UIKit
+import SnapKit
+
+final class AppHeaderView: UIView {
+    
+    private let imageView = UIImageView()
+    private let titleLabel = UILabel()
+    private let subtitleLabel = UILabel()
+    private let openButton = UIButton(configuration: .filled())
+    private let shareButton = UIButton(configuration: .plain())
+    private let labelStackView = UIStackView()
+    private let buttonStackView = UIStackView()
+
+    
+    init(title: String, subtitle: String) {
+        super.init(frame: .zero)
+        
+        setStyle(title: title, subtitle: subtitle)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setStyle(title: String, subtitle: String) {
+        backgroundColor = .systemBackground
+        
+        imageView.image = UIImage(named: "toss")
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 30
+        imageView.layer.borderWidth = 1
+        imageView.layer.borderColor = UIColor.systemGray4.cgColor
+        
+        titleLabel.text = title
+        titleLabel.font = .systemFont(ofSize: 20, weight: .bold)
+        
+        subtitleLabel.text = subtitle
+        subtitleLabel.textColor = .systemGray
+        subtitleLabel.font = .systemFont(ofSize: 16, weight: .semibold)
+        
+        openButton.configuration = {
+            var config = UIButton.Configuration.filled()
+            config.cornerStyle = .capsule
+            config.baseForegroundColor = .white
+            config.baseBackgroundColor = .systemBlue
+            
+            let attributes = AttributeContainer([
+                .font: UIFont.systemFont(ofSize: 18, weight: .semibold)
+            ])
+            config.attributedTitle = AttributedString("열기", attributes: attributes)
+            
+            return config
+        }()
+        
+        shareButton.configuration = {
+            var config = UIButton.Configuration.plain()
+            config.image = UIImage(systemName: "square.and.arrow.up")
+            config.preferredSymbolConfigurationForImage = .init(weight: .bold)
+            config.baseForegroundColor = .systemBlue
+
+            return config
+        }()
+        
+        labelStackView.configureStackView(spacing: 3)
+        buttonStackView.configureStackView(axis: .horizontal, distribution: .equalSpacing)
+    }
+    
+    func setUI() {
+        labelStackView.addArrangedSubviews(titleLabel, subtitleLabel)
+        buttonStackView.addArrangedSubviews(openButton, shareButton)
+        
+        
+        
+        self.addSubviews(imageView, labelStackView, buttonStackView)
+    }
+    
+    func setLayout() {
+        imageView.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.size.equalTo(120)
+        }
+        
+        openButton.snp.makeConstraints {
+            $0.width.equalTo(90)
+            $0.height.equalTo(32)
+        }
+        
+        shareButton.snp.makeConstraints {
+            $0.size.equalTo(32)
+        }
+        
+        labelStackView.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.top)
+            $0.leading.equalTo(imageView.snp.trailing).offset(16)
+        }
+        
+        buttonStackView.snp.makeConstraints {
+            $0.bottom.equalTo(imageView.snp.bottom)
+            $0.leading.equalTo(labelStackView.snp.leading)
+            $0.trailing.equalToSuperview()
+        }
+    }
+}
+
+#if DEBUG
+import SwiftUI
+
+#Preview {
+    AppHeaderView(title: "토스", subtitle: "금융이 쉬워진다").toPreview()
+        .frame(width: UIScreen.main.bounds.width, height: 140)
+}
+#endif
